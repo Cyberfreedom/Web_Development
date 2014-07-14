f733e1b75c85fe599390c26538b06c430a752cd3
JOHN <johnautomation2@gmail.com>
2014-07-14 17:53:58 +0600

    JavaScript onload onclick Event Advanced Project

--- TutsPlus JavaScript Fundamental/public_html/Lesson16/script.js
+++ TutsPlus JavaScript Fundamental/public_html/Lesson16/script.js
@@ -1,16 +1,50 @@
-(function() {
+window.onload = function() {
 
-var buttons = document.getElementsByTagName("button");
+    function exercise160() {
+        var buttons = document.getElementsByTagName("button");
 
-for (var i = 0, len = buttons.length; i < len; i = i + 1) {
-    buttons[i].onclick = function() {
-        var className = this.innerHTML.toLowerCase();
-        
-        document.body.className = className;
-    };
-    
-    buttons[i].onclick = function() {};
-}
+        for (var i = 0, len = buttons.length; i < len; i = i + 1) {
+            buttons[i].onclick = function() {
+                var className = this.innerHTML.toLowerCase();
 
-}());
+                document.body.className = className;
+            };
 
+//            buttons[i].onclick = function() {
+//                var className = this.innerHTML.toLowerCase();
+//                document.body.className = className;
+//            };
+        }
+
+    } //End of Exercise 160-------------------------------------------- 
+    function exercise161() {
+        var box = document.getElementById("box");
+        box.className = "box";
+        box.innerHTML = "Readey";
+        box.onclick = function() {
+            if (box.className === "box") {
+                this.className = "box active";
+                box.innerHTML = "ON";
+            } else {
+                this.className = "box";
+                box.innerHTML = "OFF";
+            }
+        };
+
+    } //End of Exercise 161--------------------------------------------
+    function exercise162() {
+        var button = document.getElementsByTagName("button");
+        var len = button.length;
+        for (var i = 0; i < len; i++) {
+            button[i].onclick = function() {
+                var caseChange = this.innerHTML.toLowerCase();
+                document.body.className = caseChange;
+            };
+        }
+    } //End of Exercise 160-------------------------------------------- 
+
+//    exercise160();
+    exercise161();
+    exercise162();
+
+}; // End Window Onload function-------------------------------------
