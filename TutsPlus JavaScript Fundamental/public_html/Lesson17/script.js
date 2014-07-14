6eace973294fc1d2154954241f4963ddd0d2d101
JOHN <johnautomation2@gmail.com>
2014-07-14 21:29:44 +0600

    JavaScript  Event : addEventListener, removeEventListenr, target &  preventDefault Method.

--- TutsPlus JavaScript Fundamental/public_html/Lesson17/script.js
+++ TutsPlus JavaScript Fundamental/public_html/Lesson17/script.js
@@ -1,22 +1,63 @@
-(function() {
+function exercise170() {
 
-var buttons = document.getElementsByTagName("a");
+    var buttons = document.getElementsByTagName("a");
 
-for (var i = 0, len = buttons.length; i < len; i = i + 1) {
-    /*buttons[i].onclick = function() {
-        var className = this.innerHTML.toLowerCase();
-        
-        document.body.className = className;
-    };*/
+    for (var i = 0, len = buttons.length; i < len; i = i + 1) {
+        /*buttons[i].onclick = function() {
+         var className = this.innerHTML.toLowerCase();
+         
+         document.body.className = className;
+         };*/
+
+        buttons[i].addEventListener("click", function(evt) {
+            var className = this.innerHTML.toLowerCase();
+
+            evt.preventDefault();
+
+            document.body.className = className;
+        }, false);
+    }
+
+}// End exercise 170----------------------------------------------
+function exercise171() {
+    var link = document.getElementsByTagName("a");
+    var len = link.length;
+    var myFunction = function() {
+        var change = this.innerHTML.toLowerCase();
+        document.body.className = change;
+        alert("Hello JavaScript Developer!");
+    };
+    for (var i = 0; i < len; i++) {
+        link[i].addEventListener("click", myFunction, false);
+        link[i].addEventListener("click", function(typ) {
+//            alert(typeof typ);    //display type of data.
+            alert(typ.target);      //display target Site.
+            typ.preventDefault();  //Prevent go target site. 
+        }, false);
+//        link[i].removeEventListener("click",myFunction,false);  //remove first EventListener.
+    }
+
+}// End exercise 170----------------------------------------------
+function exercise172() {
+        var massage = document.getElementById("cnt");
+    var changeMassage = function() {
+        if (massage.firstChild.nodeValue === "Bangaldesh") {
+            massage.firstChild.nodeValue = "Welcome to our beautyful Bangladesh";
+            massage.className = changed;
+        }
+        else {
+            massage.firstChild.nodeValue = "Welcome to our beautyful country!";
+        }
+    };
+    var background = function(){
+        document.body.className = "changed";
+    };
     
-    buttons[i].addEventListener("click", function(evt) {
-        var className = this.innerHTML.toLowerCase();
-        
-        evt.preventDefault();
-        
-        document.body.className = className;
-    }, false);    
+    massage.addEventListener("click",changeMassage,false);
+    massage.addEventListener("click",background,false);
+
+
 }
-
-}());
-
+//exercise170();
+//exercise171();
+exercise172();
