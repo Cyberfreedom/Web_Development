b79d68eaf723328646eb4c0c9ec6bfe654ea0a67
JOHN <johnautomation2@gmail.com>
2014-07-16 02:16:57 +0600

    JavaScript  Event : Cross Browser support addEventListener, removeEventListenr, attachEvent, srcElement  IE and Old Browser.

--- TutsPlus JavaScript Fundamental/public_html/Lesson19/eventUtility.js
+++ TutsPlus JavaScript Fundamental/public_html/Lesson19/eventUtility.js
@@ -1,4 +1,6 @@
-var eventUtility = {
+//Exercise  190 --------------------------------------------------
+/*
+ var eventUtility = {
     addEvent : function(el, type, fn) {
         if (typeof addEventListener !== "undefined") {
             el.addEventListener(type, fn, false);
@@ -35,3 +37,98 @@ var eventUtility = {
         }
     }
 };
+ */
+
+
+
+
+//Exercise 191---------------------------------------------------------
+/*
+  var addEvent = function(ev,type,fu){
+    if (typeof addEventListener !== "undefined"){
+        ev.addEventListener(type,fu,false);
+    }
+    else if(typeof attachEvent !== "undefined") {
+        ev.attachEvent("on"+ type,fu);
+    }
+    else {
+        ev["on" + type] = fu;
+    }
+ };
+ 
+ var removeEvent = function(ev,type,fu){
+     if (typeof removeEventListener !== "undefined"){
+         ev.removeEventListener(type,fu,false);
+     }
+     else if(typeof detachEvent !== "undefined"){
+         ev.detachEvent("on"+type,fu);
+     }
+     else{
+         el["on" + type] = null;
+     }
+ };
+ 
+ var getTarget = function(evtp){
+    if (typeof evtp.target !== "undefined"){
+        return evtp.target;
+    }
+    else{
+        return evtp.srcElement;
+    }
+ };
+ 
+ var preventDiefault = function(evt){
+     if (typeof evt.preventDefault !== "undefined"){
+     evt.preventDefault();
+     }
+     else{
+         evt.returnValue = false;
+     }
+  };
+ */
+
+
+var eventUtilitys = {
+   addEvent : function(ev,type,fu){
+     if (typeof addEventListener !== "undefined"){
+        ev.addEventListener(type,fu,false);
+    }
+    else if(typeof attachEvent !== "undefined") {
+        ev.attachEvent("on"+ type,fu);
+    }
+    else {
+        ev["on" + type] = fu;
+    }
+ },
+ 
+   removeEvent : function(ev,type,fu){
+     if (typeof removeEventListener !== "undefined"){
+         ev.removeEventListener(type,fu,false);
+     }
+     else if(typeof detachEvent !== "undefined"){
+         ev.detachEvent("on"+type,fu);
+     }
+     else{
+         el["on" + type] = null;
+     }
+ },
+ 
+   getTarget : function(evtp){
+    if (typeof evtp.target !== "undefined"){
+        return evtp.target;
+    }
+    else{
+        return evtp.srcElement;
+    }
+  },
+ 
+   preventDiefault : function(evt){
+     if (typeof evt.preventDefault !== "undefined"){
+     evt.preventDefault();
+     }
+     else{
+         evt.returnValue = false;
+     }
+  }
+};
+
